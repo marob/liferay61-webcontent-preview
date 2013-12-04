8d52528ae4b05c951d7e1f9acae789fe74ea69d9
marob <marob@smile.fr>
2013-12-04 14:00:22 +0100

    Refactoring + logs + fix for the esigate.properties missing bug + hooking "view" cmd in addition of "preview" cmd (by default) + allowing hooking cmds override in portal-ext.properties

--- webcontent-preview-hooks/preview-hook/src/main/java/fr/smile/dirtech/liferay/preview/ViewArticleContentAction.java
+++ webcontent-preview-hooks/preview-hook/src/main/java/fr/smile/dirtech/liferay/preview/ViewArticleContentAction.java
@@ -1,21 +1,5 @@
 package fr.smile.dirtech.liferay.preview;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.commons.lang3.StringUtils;
-import org.apache.http.HttpEntityEnclosingRequest;
-import org.esigate.Driver;
-import org.esigate.DriverFactory;
-import org.esigate.Parameters;
-import org.esigate.aggregator.AggregateRenderer;
-import org.esigate.esi.EsiRenderer;
-import org.esigate.servlet.HttpServletMediator;
-
 import com.liferay.portal.kernel.struts.BaseStrutsAction;
 import com.liferay.portal.kernel.struts.StrutsAction;
 import com.liferay.portal.kernel.util.Constants;
@@ -25,74 +9,106 @@ import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.service.LayoutLocalServiceUtil;
 import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portal.util.PortalUtil;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpEntityEnclosingRequest;
+import org.esigate.Driver;
+import org.esigate.DriverFactory;
+import org.esigate.Parameters;
+import org.esigate.aggregator.AggregateRenderer;
+import org.esigate.esi.EsiRenderer;
+import org.esigate.servlet.HttpServletMediator;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.*;
 
 /**
  * Overrides the default ViewArticleContentAction
- * Calls the original action, and in case of article preview, gets the original layout and inject the article new content via Esigate
- *
+ * Calls the original action, and in case of article preview, gets the original layout and inject the article new
+ * content via Esigate
  */
 public class ViewArticleContentAction extends BaseStrutsAction {
 
-	/* (non-Javadoc)
-	 * @see com.liferay.portal.kernel.struts.BaseStrutsAction#execute(com.liferay.portal.kernel.struts.StrutsAction, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
-	 */
-	@Override
-	public String execute(StrutsAction originalStrutsAction,
-			HttpServletRequest request, HttpServletResponse response)
-			throws Exception {
+    private static final String JOURNAL_PREVIEW_CMD = "journal.preview.cmd";
+    private static final List<String> JOURNAL_PREVIEW_CMD_DEFAULT = new ArrayList<String>(Arrays.asList("preview",
+            "view"));
+    private static final String JOURNAL_PREVIEW_URL_BASE = "journal.preview.url.base";
+    private static final String JOURNAL_PREVIEW_URL_BASE_DEFAULT = "http://localhost:8080";
 
-		String originalForward = originalStrutsAction
-				.execute(request, response);
-		String cmd = ParamUtil.getString(request, Constants.CMD);
-		
-		if (Constants.PREVIEW.equals(cmd)) {
-			ThemeDisplay td = (ThemeDisplay) request
-					.getAttribute(WebKeys.THEME_DISPLAY);
-			long originalPlid = td.getPlid();
-			String page = PortalUtil.getLayoutActualURL(LayoutLocalServiceUtil
-					.getLayout(originalPlid));
-			Driver driver = DriverFactory.getInstance();
+    private static Log _log = LogFactory.getLog(ViewArticleContentAction.class);
 
-			Properties props = getEsigateProperties();
+    /* (non-Javadoc)
+     * @see com.liferay.portal.kernel.struts.BaseStrutsAction#execute(com.liferay.portal.kernel.struts.StrutsAction, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+     */
+    @Override
+    public String execute(StrutsAction originalStrutsAction, HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
 
-			DriverFactory.configure(props);
-			HttpEntityEnclosingRequest httpRequest = new HttpServletMediator(
-					request, response, request.getSession().getServletContext())
-					.getHttpRequest();
-			Map<String, String> parameters = new HashMap<String, String>();
+        _log.debug("Entering in webcontent-preview-hook");
 
-			EsiRenderer esiRenderer = new EsiRenderer();
-			Map<String, CharSequence> fragments = new HashMap<String, CharSequence>();
-			fragments.put("article_preview",
-					((String) request.getAttribute("JOURNAL_ARTICLE_CONTENT")));
-			esiRenderer.setFragmentsToReplace(fragments);
-			
-			driver.render(page, parameters, response.getWriter(), httpRequest,
-					new AggregateRenderer(), esiRenderer);
-			return null;
-		}
+        String originalForward = originalStrutsAction.execute(request, response);
+        String cmd = ParamUtil.getString(request, Constants.CMD);
 
-		return originalForward;
 
-	}
+        List<String> journalPreviewCmd = Arrays.asList(PropsUtil.getArray(JOURNAL_PREVIEW_CMD));
+        if (CollectionUtils.isEmpty(journalPreviewCmd)) {
+            journalPreviewCmd = JOURNAL_PREVIEW_CMD_DEFAULT;
+        }
+        _log.debug(JOURNAL_PREVIEW_CMD + "=" + StringUtils.join(journalPreviewCmd, ", "));
 
-	/**
-	 * Builds Esigate configuration
-	 * @return
-	 */
-	private Properties getEsigateProperties() {
-		Properties props = new Properties();
-		String remoteUrlBase = PropsUtil.get("journal.preview.url.base");
-		if (StringUtils.isEmpty(remoteUrlBase)) {
-			remoteUrlBase = "http://localhost:8080";
-		}
-		props.put(Parameters.REMOTE_URL_BASE.name, remoteUrlBase);
-		props.put(Parameters.PRESERVE_HOST.name, "true");
-		props.put(Parameters.USE_CACHE.name, "false");
-		props.put(Parameters.FILTER.name,
-				"org.esigate.filter.CookieForwardingFilter");
-		props.put(Parameters.FORWARD_COOKIES.name, "*");
-		return props;
-	}
+        _log.debug("cmd=" + cmd);
+        if (journalPreviewCmd.contains(cmd)) {
+            _log.debug("Applying webcontent-preview-hook");
+
+            ThemeDisplay td = (ThemeDisplay) request.getAttribute(WebKeys.THEME_DISPLAY);
+            long originalPlid = td.getPlid();
+            String page = PortalUtil.getLayoutActualURL(LayoutLocalServiceUtil.getLayout(originalPlid));
+
+            Properties props = getEsigateProperties();
+            DriverFactory.configure(props);
+            Driver driver = DriverFactory.getInstance();
+
+            HttpEntityEnclosingRequest httpRequest = new HttpServletMediator(request, response,
+                    request.getSession().getServletContext()).getHttpRequest();
+            Map<String, String> parameters = new HashMap<String, String>();
+
+            EsiRenderer esiRenderer = new EsiRenderer();
+            Map<String, CharSequence> fragments = new HashMap<String, CharSequence>();
+            fragments.put("article_preview", ((String) request.getAttribute("JOURNAL_ARTICLE_CONTENT")));
+            esiRenderer.setFragmentsToReplace(fragments);
+
+            driver.render(page, parameters, response.getWriter(), httpRequest,
+                    new AggregateRenderer(), esiRenderer);
+            return null;
+        }
+
+        return originalForward;
+
+    }
+
+    /**
+     * Builds Esigate configuration
+     *
+     * @return
+     */
+    private Properties getEsigateProperties() {
+        Properties props = new Properties();
+
+        String remoteUrlBase = PropsUtil.get(JOURNAL_PREVIEW_URL_BASE);
+        if (StringUtils.isEmpty(remoteUrlBase)) {
+            remoteUrlBase = JOURNAL_PREVIEW_URL_BASE_DEFAULT;
+        }
+        _log.debug(JOURNAL_PREVIEW_URL_BASE + "=" + remoteUrlBase);
+
+        props.put(Parameters.REMOTE_URL_BASE.name, remoteUrlBase);
+        props.put(Parameters.PRESERVE_HOST.name, "true");
+        props.put(Parameters.USE_CACHE.name, "false");
+        props.put(Parameters.FILTER.name, "org.esigate.filter.CookieForwardingFilter");
+        props.put(Parameters.FORWARD_COOKIES.name, "*");
+        return props;
+    }
 
 }
